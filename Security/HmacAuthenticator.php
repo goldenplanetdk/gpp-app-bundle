578bfc5994cda2f305f940a4a34a9fcf93a7aa7a
Eugene Stepanov <evgeniy@goldenplanet.com>
2023-04-19 17:39:59 +0300

    fixed username pass

--- Security/HmacAuthenticator.php
+++ Security/HmacAuthenticator.php
@@ -69,7 +69,7 @@ class HmacAuthenticator extends AbstractGuardAuthenticator
         // The "username" in this case is the apiToken, see the key `property`
         // of `your_db_provider` in `security.yaml`.
         // If this returns a user, checkCredentials() is called next:
-        return $userProvider->loadUserByUsername($credentials);
+        return $userProvider->loadUserByUsername($username);
     }
 
     public function checkCredentials($credentials, UserInterface $user): bool
