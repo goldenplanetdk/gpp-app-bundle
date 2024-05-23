91924ba194e1e6b26f620731c1c5e693d2c7033b
Eugene Stepanov <evgeniy@goldenplanet.com>
2024-05-23 11:36:39 +0300

    Upgrade for SF 6.x

--- Security/HmacAuthenticator.php
+++ Security/HmacAuthenticator.php
@@ -1,27 +1,26 @@
 <?php
 
-namespace GoldenPlanet\PriceMonitoring\Infrastructure\AppBundle\Security;
+namespace GoldenPlanet\GPPAppBundle\Security;
 
-use Doctrine\ORM\EntityManagerInterface;
 use GoldenPlanet\Gpp\App\Installer\Validator\HmacValidator;
-use GoldenPlanet\PriceMonitoring\Domain\Entity\User;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
 use Symfony\Component\Security\Http\SecurityRequestAttributes;
 
-class HmacCustomAuthenticator extends AbstractAuthenticator
+class HmacAuthenticator extends AbstractAuthenticator
 {
     public function __construct(
         private readonly HmacValidator $validator,
-        private readonly EntityManagerInterface $entityManager,
+        private readonly UserProviderInterface $userProvider
     ) {
     }
 
@@ -49,7 +48,7 @@ class HmacCustomAuthenticator extends AbstractAuthenticator
                     'This action needs a valid hmac sign'
                 );
             }
-            return $this->entityManager->getRepository(User::class)->findOneBy(['username' => $shop]);
+            return $this->userProvider->loadUserByIdentifier($shop);
         });
         return new SelfValidatingPassport($userBadge);
     }
